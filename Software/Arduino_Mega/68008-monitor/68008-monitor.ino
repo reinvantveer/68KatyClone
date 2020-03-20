c75aa5b69205bdf6439dee11c142ffcb65752b49
Rein van 't Veer <reinvantveer@gmail.com>
2020-03-20 19:49:36 +0100

    fix reverse pin order, add address mapping

--- Software/Arduino_Mega/68008-monitor/68008-monitor.ino
+++ Software/Arduino_Mega/68008-monitor/68008-monitor.ino
@@ -2,15 +2,16 @@
 // In its adapted form, it supports the 20-bit address bus of the 68008 however, instead of the 16 lines of the 6502.
 // Check out his video on using the data logger on https://youtu.be/LnzuMJLZRdU?t=513
 
+//Mapping for address pins   A0  A1  A2  A3  A4  A5  A6  A7  A8  A9  A10 A11 A12 A13 A14 A15 A16 A17 A18 A19
 const char ADDRESS_PINS[] = {22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 53, 51, 49, 47};
-const char DATA_PINS[] = {31, 33, 35, 37, 39, 41, 43, 45};
+//Mapping for data pins      D0  D1  D2  D3  D4  D5  D6  D7
+const char DATA_PINS[] =    {31, 33, 35, 37, 39, 41, 43, 45};
 
 //#define CLOCK 29
 #define READ_WRITE 27
 // DTACK needs to be on a special-purpose interruptable pin: 
 // See https://www.arduino.cc/reference/en/language/functions/external-interrupts/attachinterrupt/
 #define DTACK 21
-
 
 void setup() {
   Serial.begin(57600);
@@ -19,6 +20,7 @@ void setup() {
   Serial.println();
   Serial.print("Watching address bus of size: ");
   Serial.println(sizeof(ADDRESS_PINS));
+
   for (int n = 0; n < sizeof(ADDRESS_PINS); n++) {
     pinMode(ADDRESS_PINS[n], INPUT);
     Serial.println("Address pin " + String(int(ADDRESS_PINS[n])) + " set as input");
@@ -28,7 +30,7 @@ void setup() {
   Serial.println();
   Serial.print("Watching data bus of size: ");
   Serial.println(sizeof(DATA_PINS));
-  for (int n = 0; n < sizeof(DATA_PINS); n++) {
+  for (int n = 0; n < sizeof(DATA_PINS) ; n++) {
     pinMode(DATA_PINS[n], INPUT);
     Serial.println("Data pin " + String(int(DATA_PINS[n])) + " set as input");
   }
@@ -52,7 +54,7 @@ void readBuses() {
   char output[15];
 
   unsigned int address = 0;
-  for (int n = 0; n < sizeof(ADDRESS_PINS); n++) {
+  for (int n = sizeof(ADDRESS_PINS) - 1; n >= 0; n--) {
     int bit = digitalRead(ADDRESS_PINS[n]) ? 1 : 0;
     Serial.print(bit);
     address = (address << 1) + bit;
@@ -61,7 +63,7 @@ void readBuses() {
   Serial.print("   ");
   
   unsigned int data = 0;
-  for (int n = 0; n < sizeof(DATA_PINS); n += 1) {
+  for (int n = sizeof(DATA_PINS) - 1; n >= 0; n --) {
     int bit = digitalRead(DATA_PINS[n]) ? 1 : 0;
     Serial.print(bit);
     data = (data << 1) + bit;
