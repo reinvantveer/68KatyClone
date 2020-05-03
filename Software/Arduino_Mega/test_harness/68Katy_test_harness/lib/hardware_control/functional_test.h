0ad3c2c18dee0ae5c0b22188e48f9dc60398ffa8
Rein van 't Veer <reinvantveer@gmail.com>
2020-05-03 18:52:32 +0200

    improve test output as single line with percentage complete

--- Software/Arduino_Mega/test_harness/68Katy_test_harness/lib/hardware_control/functional_test.h
+++ Software/Arduino_Mega/test_harness/68Katy_test_harness/lib/hardware_control/functional_test.h
@@ -21,27 +21,27 @@ void freerunTest() {
   // After 8 data bus cycles, the actual freerunning can begin
   // We're jumping the odd addresses since our NOP operation takes two data bus cycles
   unsigned long address_space = 1ul << ADDRESS_BUS_SIZE; // pow(2, 20); is slower
+  Serial.println("Performing free-running test, using NOP (no operation) instructions");
   Serial.println("Iterating over address space of size " + String(address_space));
 
   for (unsigned long address_check = 0; address_check < address_space; address_check += 2) {
     address = read_address_bus();
 
     if (!(address_check & 0x0FFF)) {
-      Serial.println("Checking address " + String(address_check));
+      int percentage_complete = address_check * 100 / address_space;
+      Serial.print("\rChecking address " + String(address_check) + " (" + String(percentage_complete) + "%) ");
+      // Print the contents of the address bus
+      Serial.print(" Address binary: ");
+      for (int pin = ADDRESS_BUS_SIZE - 1; pin >= 0; pin--) {
+        unsigned int bit = digitalRead(address_pins[pin]);
+        Serial.print(String(bit));
+        // Serial.println("Address line " + String(address_lines[pin]) + " is " + String(bit));
+      }
 
       // Validate that we're advancing two addresses at a time
       // If not: print the encountered address discrepancy and return
       if (address_check != address) {
         Serial.println("Error: expected address: " + String(address_check) + " got " + String(address));
-
-        // Print the contents of the address bus
-        Serial.print("Address binary: ");
-        for (int pin = ADDRESS_BUS_SIZE - 1; pin >= 0; pin--) {
-          unsigned int bit = digitalRead(address_pins[pin]);
-          Serial.print(String(bit));
-          // Serial.println("Address line " + String(address_lines[pin]) + " is " + String(bit));
-        }
-        Serial.print("\n");
 
         // Prematurely end the free-running function
         return;
@@ -59,5 +59,6 @@ void freerunTest() {
     dtack_pulse();
   }
 
+  Serial.println();
   Serial.println("Freerunning test OK!");
 }
