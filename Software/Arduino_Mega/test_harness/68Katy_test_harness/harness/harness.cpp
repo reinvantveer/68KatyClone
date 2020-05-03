9c0768eaa7eccece3f531de73e4e4c11d273f34a
Rein van 't Veer <reinvantveer@gmail.com>
2020-05-03 15:32:43 +0200

    extract more hardware functions to header, increase dtack_pulse to 10 ms each

--- Software/Arduino_Mega/test_harness/68Katy_test_harness/harness/harness.cpp
+++ Software/Arduino_Mega/test_harness/68Katy_test_harness/harness/harness.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include "functional_test.h"
 
 /*
 A test harness for checking chip and board functionality for 68k projects.
@@ -8,171 +9,6 @@ You can either use this file from PlatformIO (https://platformio.org) or simple 
 But I much prefer PlatformIO for better IDE functionality, faster compilation, autocomplete etc. 
 
 */
-
-
-// Pin mapping for address lines
-#define ADDRESS_BUS_SIZE 20
-const String address_lines[ADDRESS_BUS_SIZE] = {
-    "A0",  "A1",  "A2",  "A3",  "A4",  "A5",  "A6",  "A7",  "A8",  "A9",
-    "A10", "A11", "A12", "A13", "A14", "A15", "A16", "A17", "A18", "A19"
-};
-// These lines correspond with the following Arduino pins:
-const unsigned int address_pins[ADDRESS_BUS_SIZE] = {22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 53, 51, 49, 47};
-
-//Mapping for data pins
-#define DATA_BUS_SIZE 8
-const String data_lines[DATA_BUS_SIZE] = {"D0", "D1", "D2", "D3", "D4", "D5", "D6", "D7"};
-const unsigned int data_pins[DATA_BUS_SIZE] =  {31,   33,   35,   37,   39,   41,  43,  45};
-
-// Assign the Data Acknowledge pin
-#define DTACK 27
-
-// Assign the reset circuit pin
-#define RESET 29
-
-// Set the serial communication baudrate
-#define BAUDRATE 115200
-
-// Sets the address pins as inputs
-void address_pins_as_inputs() {
-    for (int n = 0; n < ADDRESS_BUS_SIZE; ++n) {
-      pinMode(address_pins[n], INPUT);
-      Serial.println(
-        "Address line " + address_lines[n] + " set as input to Arduino pin " + String(int(address_pins[n]))
-      );
-  }
-}
-
-// Sets the data pins as outputs
-void data_pins_as_outputs() {
-  for (int n = 0; n < DATA_BUS_SIZE ; ++n) {
-    pinMode(data_pins[n], OUTPUT);
-    Serial.println(
-      "Data line " + data_lines[n] + " set as output to Arduino pin " + String(int(data_pins[n]))
-    );
-  }
-}
-
-
-// Set up the data acknowledge signal
-void dtack_setup() {
-  pinMode(DTACK, OUTPUT);
-  // DTACK is an active-low signal, so we're keeping it high
-  digitalWrite(DTACK, HIGH);
-}
-
-
-// Set up the data acknowledge signal
-void reset_setup() {
-  pinMode(RESET, OUTPUT);
-  // RESET is an active-low signal, so we're pulling it low
-  digitalWrite(RESET, LOW);
-  // The reset needs some time to be active
-  delay(1);
-  // Then we're keeping it high
-  digitalWrite(RESET, HIGH);
-}
-
-
-// Reads the address bus for the requested address and returns it as a number
-unsigned long read_address_bus() {
-  unsigned long address = 0;
-  unsigned long offset = 1;
-
-  for (int pin = 0; pin < ADDRESS_BUS_SIZE; ++pin) {
-      int bit = digitalRead(address_pins[pin]);
-      address += bit * offset;
-      offset <<= 1;
-  }
-
-  return address;
-}
-
-
-// Writes data to the data bus
-void data_write(unsigned int data) {
-  // Serial.println("Data: " + String(data));
-  for (unsigned int pin = 0; pin < DATA_BUS_SIZE ; pin++) {
-    bool masked = data & 1 << pin;
-    // Serial.println("Pin/bit " + String(data_lines[pin]) + " value: " + String(masked));
-    digitalWrite(data_pins[pin], masked);
-  }
-}
-
-
-// Sends a single DTACK signal to advance the chip through its instructions
-void dtack_pulse() {
-  // Set DTACK low to active
-  digitalWrite(DTACK, LOW);
-  // Wait for a split second
-  delayMicroseconds(10);
-  digitalWrite(DTACK, HIGH);
-  // Wait for a split second
-  delayMicroseconds(10);
-}
-
-
-// Runs a free-running loop over the entire address space of the Motorola 68k
-void freerunTest() {
-  Serial.println("Checking for correct start address from fresh reset.");
-  unsigned long address = read_address_bus();
-
-  if (address > 0) {
-    Serial.print("Error: initial address " + String(address) + " was expected to be 0. There must be something wrong with the reset circuit");
-    return;
-  }
-  Serial.println("Start address OK: " + String(address));
-
-  // Gloss over the first 8 data bus read cycles to account for the start and reset vectors of the chip
-  Serial.println("Booting: iterating over start and reset vectors in 1024 + 8 cycles.");
-  for (unsigned long address_check = 0; address_check < 8; address_check++) {
-    dtack_pulse();
-  }
-
-  // After 8 data bus cycles, the actual freerunning can begin
-  // We're jumping the odd addresses since our NOP operation takes two data bus cycles
-  unsigned long address_space = pow(2, 20);
-  Serial.println("Iterating over address space of size " + String(address_space));
-
-  for (unsigned long address_check = 0; address_check < address_space; address_check += 2) {
-    address = read_address_bus();
-
-    if (address_check % 4096 == 0) {
-      Serial.println("Checking address " + String(address_check));
-
-      // Validate that we're advancing two addresses at a time
-      // If not: print the encountered address discrepancy and return
-      if (address_check != address) {
-        Serial.println("Error: expected address: " + String(address_check) + " got " + String(address));
-
-        // Print the contents of the address bus
-        Serial.print("Address binary: ");
-        for (int pin = ADDRESS_BUS_SIZE - 1; pin >= 0; pin--) {
-          unsigned int bit = digitalRead(address_pins[pin]);
-          Serial.print(String(bit));
-          // Serial.println("Address line " + String(address_lines[pin]) + " is " + String(bit));
-        }
-        Serial.print("\n");
-
-        // Prematurely end the free-running function
-        return;
-      }
-
-    }
-
-
-    // NOP high byte
-    data_write(0x004E);
-    dtack_pulse();
-
-    // NOP low byte
-    data_write(0x0071);
-    dtack_pulse();
-  }
-
-  Serial.println("Freerunning test OK!");
-}
-
 
 
 void setup() {
@@ -196,5 +32,5 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  // dtack_pulse();
+  dtack_pulse();
 }
