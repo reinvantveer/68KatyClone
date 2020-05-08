b1288e6f2e9b1b8e37ff10846a6edc2b529b2e0d
Rein van 't Veer <reinvantveer@gmail.com>
2020-05-08 19:59:04 +0200

    add selection menu

--- Software/Arduino_Mega/test_harness/68Katy_test_harness/src/main.cpp
+++ Software/Arduino_Mega/test_harness/68Katy_test_harness/src/main.cpp
@@ -10,7 +10,12 @@ You can either use this file from PlatformIO (https://platformio.org) or simple 
 But I much prefer PlatformIO for better IDE functionality, faster compilation, autocomplete etc. 
 
 */
-
+void print_menu() {
+    // Show the selection menu
+  Serial.println("\nThe board is ready for testing. Please select one of the following tests:");
+  Serial.println("\t[1] Freerunning test");
+  Serial.println("\t[2] ROM test");
+}
 
 void setup() {
   // Set up the serial communication with the host machine interfacing with Arduino
@@ -30,14 +35,27 @@ void setup() {
   // Allow the chip to reset
   delay(1000);
 
-  // Start testing the free-running capability of the m68k
-  freerun_test();
-
-  // Test the ROM functionality
-  rom_test();
+  print_menu();
 }
 
 void loop() {
+  int incomingByte = 0; // for incoming serial data
+
+  // Only when you receive data:
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
+
+    // say what you got:
+    char chosen_option = (char)incomingByte;
+    Serial.print("Chosen option: ");
+    Serial.println(chosen_option);
+
+    if (chosen_option == '1') freerun_test();  // Start testing the free-running capability of the m68k
+    if (chosen_option == '2') rom_test();      // Test the ROM functionality
+    print_menu();
+  }
+  
   // For performance measurement: just keep acknowledging the data bus cycles and reading the address
   dtack_pulse(); // 0.02 ms
   read_address_bus(); // 0.109 ms
