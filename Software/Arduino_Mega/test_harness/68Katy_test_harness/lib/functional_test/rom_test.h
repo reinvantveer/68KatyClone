90c3404a65e9f9794326ed8f8869ea5a424edd57
Rein van 't Veer <reinvantveer@gmail.com>
2020-05-09 16:45:18 +0200

    make rom hash function, set pins, reset

--- Software/Arduino_Mega/test_harness/68Katy_test_harness/lib/functional_test/rom_test.h
+++ Software/Arduino_Mega/test_harness/68Katy_test_harness/lib/functional_test/rom_test.h
@@ -1,16 +1,45 @@
 #include <Arduino.h>
 
+#ifndef HARDWARE_CONTROL
+#include <hardware_control.h>
+#endif
+
 unsigned long hash_rom_space() {
-  return 0; // Dummy value for now
+  /*
+  The ROM test goes through the address space of the ROM chip with 4096 bit jumps.
+  Each time it jumps to the next location, it reads the byte stored there and increases a hash value with the data byte.
+  */
+
+  unsigned long rom_address_space_start = 0x00000;
+  unsigned long rom_address_space_end = 0x77FFF;
+  unsigned int address_increment = 4096;
+  
+  unsigned long hash_value = 0;
+
+  for (unsigned long address = rom_address_space_start; address <= rom_address_space_end; address += address_increment) {
+    write_address_bus(address);
+    delay(50);
+    unsigned int data = read_data_bus();
+    Serial.print("\rAddress " + String(address) + " has data " + String(data));
+    hash_value += data;
+    delay(50);
+  }
+
+  Serial.println();
+  return hash_value; // Dummy value for now
 }
 
 void rom_test() {
   /*
-  The ROM test goes through the address space of the ROM chip with 4096 bit jumps.
-  Each time it jumps to the next location, it reads the byte stored there and multiplies a hash value.
+  The ROM test gathers a hash value for part of the contents of the ROM. 
   The test then repeats this process, to a second hash.
   Once the entire ROM address space is visited twice, the hash values should not be 0 and should be equal.
   */
+
+  // First of all, reset the m68k system
+  reset_setup();
+  data_pins_as_inputs();
+  address_pins_as_outputs();
 
   unsigned long hash_first_pass = hash_rom_space();
   if (! hash_first_pass) {
@@ -18,6 +47,7 @@ void rom_test() {
     return;
   }
 
+  reset_setup();
   unsigned long hash_second_pass = hash_rom_space();
   if (! hash_second_pass) {
     Serial.println("Error! ROM hash for the second pass was 0.");
@@ -25,8 +55,10 @@ void rom_test() {
   }
 
   if (!(hash_first_pass == hash_second_pass)) {
-    Serial.println("Error! ROM hashes of both passes was not identical.");
+    Serial.println("Error! ROM hash of pass 1: " + String(hash_first_pass) + " was not identical to pass 2: " + String(hash_second_pass));
     return;
   }
+
+  Serial.println("ROM test success!");
 
 }
