99e06752f1b670daca7781b6a2fbaebf200e700f
Rein van 't Veer <reinvantveer@gmail.com>
2020-05-13 19:41:41 +0200

    WIP add more debugging to rom test

--- Software/Arduino_Mega/test_harness/68Katy_test_harness/lib/functional_test/rom_test.h
+++ Software/Arduino_Mega/test_harness/68Katy_test_harness/lib/functional_test/rom_test.h
@@ -4,6 +4,7 @@
 #include <hardware_control.h>
 #endif
 
+
 unsigned long hash_rom_space() {
   /*
   The ROM test goes through the address space of the ROM chip with 4096 bit jumps.
@@ -11,18 +12,85 @@ unsigned long hash_rom_space() {
   */
 
   unsigned long rom_address_space_start = 0x00000;
-  unsigned long rom_address_space_end = 0x77FFF;
+  // unsigned long rom_address_space_end = 0x77FFF;
+  unsigned long rom_address_space_end = 0x5FFFF;
+
+  // This is the space between samples we're taking from the ROM
   unsigned int address_increment = 4096;
   
+  // This is going to contain our hash value for the sampled content
   unsigned long hash_value = 0;
 
-  for (unsigned long address = rom_address_space_start; address <= rom_address_space_end; address += address_increment) {
-    write_address_bus(address);
-    delay(50);
-    unsigned int data = read_data_bus();
-    Serial.print("\rAddress " + String(address) + " has data " + String(data));
-    hash_value += data;
-    delay(50);
+  Serial.println("Checking for correct start address from fresh reset.");
+  unsigned long address = read_address_bus();
+
+  if (address > 0) {
+    Serial.println("Error: initial address " + String(address) + " was expected to be 0. There must be something wrong with the reset circuit or the DTACK is enabled");
+    // Abort
+    return 0;
+  }
+  Serial.println("Start address OK: address " + String(address));
+
+
+  for (unsigned long address = rom_address_space_start; address < rom_address_space_end; address += address_increment) {
+    // We checked that the m68k chip is reset at address 0 and we'll assume that the stack pointer and program counter have been initialized 
+
+    // We're going to use this moment to read the contents of the data bus for the low byte
+    data_pins_as_inputs();
+    delay(100);
+    unsigned int data_high = read_data_bus();
+    Serial.println("Data high: " + String(data_high));
+
+    // We're going to replace the contents of the data bus with instructions for the chip to jump to the required address
+    data_pins_as_outputs();
+    delay(100);
+    write_data_bus(0x4E); // Basically the JMP opcode
+    delay(10);
+    dtack_pulse();
+    delay(10);
+
+    // This time, read the high byte contents of the data bus
+    data_pins_as_inputs();
+    delay(100);
+    unsigned int data_low = read_data_bus();
+    Serial.println("Data low: " + String(data_low));
+
+    // Again, we're going to replace the contents of the data bus with instructions for the chip to jump to the required address
+    data_pins_as_outputs();
+    delay(10);
+    write_data_bus(0xF9); // Mode/register setting for direct addressing, long word (four byte) address
+    delay(10);
+    dtack_pulse();
+    delay(10);
+
+    // Now, we're giving the long word value for the address to jump to
+    unsigned int address_4th_byte = address >> 24;
+    write_data_bus(address_4th_byte);
+    dtack_pulse();
+
+    unsigned int address_3rd_byte = address >> 16;
+    write_data_bus(address_3rd_byte);
+    dtack_pulse();
+    delay(10);
+
+    unsigned int address_2nd_byte = address >> 8;
+    write_data_bus(address_2nd_byte);
+    dtack_pulse();
+    delay(10);
+
+    unsigned int address_1st_byte = (int)address;
+    write_data_bus(address_1st_byte);
+    dtack_pulse();
+    delay(10);
+
+    // The m68k will now execute the JMP instruction and read the contents of the ROM at the next specified address
+    delay(5);
+    unsigned long data_long = ((unsigned long)data_high << 8) + data_low;
+
+    unsigned long read_address = read_address_bus(); 
+    Serial.println("Address " + String(address) + " at read address " + String(read_address)  + " has data " + String(data_long));
+    hash_value += data_long;
+    delay(100);
   }
 
   Serial.println();
@@ -36,10 +104,10 @@ void rom_test() {
   Once the entire ROM address space is visited twice, the hash values should not be 0 and should be equal.
   */
 
-  // First of all, reset the m68k system
+  // First of all, set the right pin modes
+  data_pins_as_outputs();
+  // Reset the m68k system
   reset_setup();
-  data_pins_as_inputs();
-  address_pins_as_outputs();
 
   unsigned long hash_first_pass = hash_rom_space();
   if (! hash_first_pass) {
@@ -48,17 +116,18 @@ void rom_test() {
   }
 
   reset_setup();
+
   unsigned long hash_second_pass = hash_rom_space();
   if (! hash_second_pass) {
     Serial.println("Error! ROM hash for the second pass was 0.");
     return;
   }
 
-  if (!(hash_first_pass == hash_second_pass)) {
+  if (hash_first_pass == hash_second_pass) {
+    Serial.println("ROM test success! Hash values for both ROM passes were identical.");
+  } else {
     Serial.println("Error! ROM hash of pass 1: " + String(hash_first_pass) + " was not identical to pass 2: " + String(hash_second_pass));
-    return;
   }
 
-  Serial.println("ROM test success!");
-
+  reset_setup();
 }
