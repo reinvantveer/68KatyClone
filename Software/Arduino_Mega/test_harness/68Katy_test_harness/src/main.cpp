8c0628a99226099064fbce296ef902f7a8a28d38
Rein van 't Veer <reinvantveer@gmail.com>
2020-05-24 14:51:06 +0200

    Move basic setup and config back to main

--- Software/Arduino_Mega/test_harness/68Katy_test_harness/src/main.cpp
+++ Software/Arduino_Mega/test_harness/68Katy_test_harness/src/main.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-#include <Mega2M68k8.h>
 #include "freerun_test.h"
 #include "rom_test.h"
 
@@ -12,20 +11,25 @@ But I much prefer PlatformIO for better IDE functionality, faster compilation, a
 
 */
 
-// Instantiate the interface board
-Mega2M68k8 interface_board; 
+#define BAUDRATE 115200
+
+// Flags for keeping track of the input or output mode of the address and data bus pins
+bool is_address_bus_in_input_mode;
+bool is_data_bus_in_input_mode;
+
+volatile int last_data_bus_contents;
 
 // Read the data bus when triggered throug a data strobe 
 void read_data_bus_triggered(){
-  interface_board.last_data_bus_contents = read_data_bus();
-  // Serial.write("The data strobe event occurred.");
+  last_data_bus_contents = read_data_bus();
+  // Serial.println("The data strobe event occurred: data " + String(read_byte));
 }
 
 void print_menu() {
     // Show the selection menu
   Serial.println("\nThe board is ready for testing. Please select one of the following tests:");
   Serial.println("\t[1] Freerunning test");
-  Serial.println("\t[2] ROM test");
+  Serial.println("\t[2] ROM test (Work in progress)");
 
   Serial.println("\nOr: perform some basic operation:");
   Serial.println("\t[3] Reset the 68k chip. The RAM contents are unaffected.");
@@ -36,24 +40,27 @@ void print_menu() {
 }
 
 void setup() {
-  interface_board = Mega2M68k8();
-
-  // Set up the DTACK pin
-  dtack_setup();
+  Serial.begin(BAUDRATE);
 
   // We're not going to use the address bus for anything else but inputs
   address_pins_as_inputs();
+  is_address_bus_in_input_mode = true;
+  
   data_pins_as_inputs();
+  is_data_bus_in_input_mode = true;
+    
+  // Set up the DTACK pin
+  dtack_setup();
 
   // Reset the chip to get a fresh readout
   reset_setup();
 
-  // Show info
+  // Show info on m68k8 to Arduino pin mappings
   print_address_pin_mapping();
   print_data_pin_mapping();
 
   print_menu();
-  attachInterrupt(digitalPinToInterrupt(DATA_STROBE_PIN), read_data_bus_triggered, FALLING);
+  attachInterrupt(digitalPinToInterrupt(DATA_STROBE_PIN), read_data_bus_triggered, RISING);
 }
 
 void loop() {
@@ -71,12 +78,12 @@ void loop() {
 
     // Tests
     if (chosen_option == '1') freerun_test();  // Start testing the free-running capability of the m68k
-    if (chosen_option == '2') rom_test();      // Test the ROM functionality
+    if (chosen_option == '2') { rom_test(); print_menu(); }     // Test the ROM functionality
 
     // Basic operation
     if (chosen_option == '3') { reset_setup(); print_menu(); }  // Reset the chip
     if (chosen_option == '4') Serial.print("\r\tAddress: " + String(read_address_bus()) + "\t\t\t\t\t"); // Print the current address on the address bus
-    if (chosen_option == '5') Serial.print("\r\tData:" + String(interface_board.last_data_bus_contents) + "\t\t\t\t\t"); // Print the last known data byte on the data bus
+    if (chosen_option == '5') Serial.print("\r\tLast known byte on data bus:" + String(last_data_bus_contents) + "\t\t\t\t\t"); // Print the last known data byte on the data bus
     if (chosen_option == '6') { dtack_pulse(); Serial.print("\r\tAcknowledged one data bus cycle"); }
 
     // Afterwards: print the menu again
